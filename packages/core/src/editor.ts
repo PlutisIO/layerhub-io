df2508d4ff26b074f7785c72783abb26735e860d
Kamil Ali <kamil.mu.ali@gmail.com>
2024-05-15 00:43:36 -0700

    fixes

--- packages/core/src/editor.ts
+++ packages/core/src/editor.ts
@@ -13,6 +13,7 @@ import { EditorState } from "./common/interfaces"
 import { defaultEditorConfig } from "./common/constants"
 import Guidelines from "./controllers/Guidelines"
 import { EditorConfig } from "@layerhub-plutis-io/types"
+import { fabric } from "fabric"
 
 export class Editor extends EventManager {
   public canvas: Canvas
@@ -48,6 +49,77 @@ export class Editor extends EventManager {
       config: this.config,
       editor: this,
     })
+    canvas.canvas.on('mouse:dblclick', function(options: any) {
+      function ungroup (group: any) {
+        groupItems = group._objects;
+        group._restoreObjectsState();
+        canvas.canvas.remove(group);
+        for (var i = 0; i < groupItems.length; i++) {
+            if(groupItems[i] != "StaticText"){
+                groupItems[i].selectable = false;
+            }                               
+            canvas.canvas.add(groupItems[i]);
+        }
+        canvas.canvas.renderAll();
+      };
+      var groupItems: any;
+      if (options.target) {
+        var thisTarget = options.target; 
+        var mousePos = canvas.canvas.getPointer(options.e);
+        var editTextbox = false;
+        var editObject;
+    
+        if (thisTarget.isType('group')) {
+          var groupPos = {
+            x: thisTarget.left,
+            y: thisTarget.top
+          }
+  
+          thisTarget.forEachObject(function(object: any, i: any) {
+            console.log(object.type);
+            if(object.type == "StaticText"){           
+              var matrix = thisTarget.calcTransformMatrix();
+              const currPoint: any = {y: object.top, x: object.left};
+              var newPoint = fabric.util.transformPoint(currPoint, matrix);
+              var objectPos = {
+                xStart: newPoint.x - (object.width * object.scaleX) / 2,//When OriginX and OriginY are centered, otherwise xStart: newpoint.x - object.width * object.scaleX etc...
+                xEnd: newPoint.x + (object.width * object.scaleX) / 2,
+                yStart: newPoint.y - (object.height * object.scaleY) / 2,
+                yEnd: newPoint.y + (object.height * object.scaleY) / 2
+              }
+
+              if ((mousePos.x >= objectPos.xStart && mousePos.x <= objectPos.xEnd) && (mousePos.y >= objectPos.yStart && mousePos.y <= objectPos.yEnd)) {
+                  
+
+                ungroup(thisTarget);
+                canvas.canvas.setActiveObject(object);
+
+                object.enterEditing();
+                object.selectAll();
+
+                editObject = object;
+                var exitEditing = true;
+
+                editObject.on('editing:exited', function (options: any) {
+                  if(exitEditing){
+                    var items: any = [];
+                    groupItems.forEach(function (obj: any) {
+                      items.push(obj);
+                      canvas.canvas.remove(obj);
+                    });
+
+                    var grp
+                    grp = new fabric.Group(items, {});
+                    canvas.canvas.add(grp);
+                    exitEditing = false;
+                  }
+                });
+              }
+            }
+          });
+        }    
+      }
+    });
     this.canvas = canvas
   }
 
